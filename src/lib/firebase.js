b67753ffbf0f6969da39a72354c2277bdae262be
Preet Bhavsar <preetbhavsar06@gmail.com>
2026-09-11 19:31:46 +0530

    Hardcode Firebase credentials

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,17 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "dummy-key-to-bypass-build-error",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "dummy",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "dummy",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "dummy",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "dummy",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "dummy",
+    apiKey: "AIzaSyDZXKKJW08dBFKWhNkaprTqmtJIBbFw_Ko",
+    authDomain: "swagat-samosa-ordering-website.firebaseapp.com",
+    projectId: "swagat-samosa-ordering-website",
+    storageBucket: "swagat-samosa-ordering-website.firebasestorage.app",
+    messagingSenderId: "802489979437",
+    appId: "1:802489979437:web:32eb45bb460bfbaaee5ea4",
 };
 
 const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApp();
+    ? initializeApp(firebaseConfig)
+    : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
